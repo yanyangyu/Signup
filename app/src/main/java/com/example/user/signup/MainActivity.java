43e45f72f21fd495d9a128d9ac80f1b2a929869f
yanyangyu <40441114@gm.nfu.edu>
2017-07-13 13:55:30 +0800

    手機連接資料庫

---
+++ app/src/main/java/com/example/user/signup/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.user.signup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etFullName, etUserName, etPassword, etPhone, etEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etFullName = (EditText) findViewById(R.id.etFullName);
+        etUserName = (EditText) findViewById(R.id.etUserName);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        etPhone = (EditText) findViewById(R.id.etPhone);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+    }
+
+    public void signup(View v) {
+        String fullName = etFullName.getText().toString();
+        String userName = etUserName.getText().toString();
+        String passWord = etPassword.getText().toString();
+        String phoneNumber = etPhone.getText().toString();
+        String emailAddress = etEmail.getText().toString();
+
+        Toast.makeText(this, "Signing up...", Toast.LENGTH_SHORT).show();
+        new SignupActivity(this).execute(fullName, userName, passWord, phoneNumber, emailAddress);
+    }
+
+
+
+    }
+
---
+++ app/src/main/java/com/example/user/signup/SignupActivity.java
@@ -0,0 +1,92 @@
+/**
+ * Created by user on 2017/7/13.
+ */
+
+
+
+//   參考 http://kb4dev.com/article/how-to-connect-android-with-php,-mysql-and-json
+//   要有資料庫 php myadmin
+
+
+package com.example.user.signup;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class SignupActivity extends AsyncTask<String, Void, String> {
+
+    private Context context;
+
+    public SignupActivity(Context context) {
+        this.context = context;
+    }
+
+    protected void onPreExecute() {
+
+    }
+
+    @Override
+    protected String doInBackground(String... arg0) {
+        String fullName = arg0[0];
+        String userName = arg0[1];
+        String passWord = arg0[2];
+        String phoneNumber = arg0[3];
+        String emailAddress = arg0[4];
+
+        String link;
+        String data;
+        BufferedReader bufferedReader;
+        String result;
+
+        try {
+            data = "?fullname=" + URLEncoder.encode(fullName, "UTF-8");
+            data += "&username=" + URLEncoder.encode(userName, "UTF-8");
+            data += "&password=" + URLEncoder.encode(passWord, "UTF-8");
+            data += "&phonenumber=" + URLEncoder.encode(phoneNumber, "UTF-8");
+            data += "&emailaddress=" + URLEncoder.encode(emailAddress, "UTF-8");
+
+            link = "http://140.130.33.150/signup.php" + data;
+            URL url = new URL(link);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            result = bufferedReader.readLine();
+            return result;
+        } catch (Exception e) {
+            return new String("Exception: " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        String jsonStr = result;
+        if (jsonStr != null) {
+            try {
+                JSONObject jsonObj = new JSONObject(jsonStr);
+                String query_result = jsonObj.getString("query_result");
+                if (query_result.equals("SUCCESS")) {
+                    Toast.makeText(context, "Data inserted successfully. Signup successful.", Toast.LENGTH_SHORT).show();
+                } else if (query_result.equals("FAILURE")) {
+                    Toast.makeText(context, "Data could not be inserted. Signup failed.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, "Couldn't connect to remote database.", Toast.LENGTH_SHORT).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Toast.makeText(context, "Error parsing JSON data.", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(context, "Couldn't get any JSON data.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}

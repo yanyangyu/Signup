1b6b5c39ac98a3433327971f06d91bf16c09c9a6
yanyangyu <40441114@gm.nfu.edu.tw>
2017-07-13 15:38:59 +0800

    signup.php
    version2

--- signup.php
+++ signup.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 $con=mysqli_connect("127.0.0.1","root","123456","member");
 if (mysqli_connect_errno($con))
 {
@@ -14,15 +12,26 @@ $passWord = $_GET['password'];
 $phoneNumber = $_GET['phonenumber'];
 $emailAddress = $_GET['emailaddress'];
 
-$result = mysqli_query($con,"INSERT INTO user (fullname, username, password, phone, email)
-          VALUES ('$fullName', '$userName', '$passWord', '$phoneNumber', '$emailAddress')");
 
-if($result == true) {
-    echo '{"query_result":"SUCCESS"}';
-}
-else{
-    echo '{"query_result":"FAILURE"}';
-}
+$sql = "SELECT id From user where phone = '$phoneNumber' or email = '$emailAddress'";
+$result = $con->query($sql);
+
+    if ($result->num_rows > 0)
+    {
+        echo '{"query_result":"Duplicate"}';
+    }
+    else
+    {
+      $result = mysqli_query($con,"INSERT INTO user (fullname, username, password, phone, email)
+      VALUES ('$fullName', '$userName', '$passWord', '$phoneNumber', '$emailAddress')");
+              if($result == true) {
+                  echo '{"query_result":"SUCCESS"}';
+                }else{
+                  echo '{"query_result":"FAILURE"}';
+               }
+    }
+
+
 mysqli_close($con); //關閉連線
 
 
